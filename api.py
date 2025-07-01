e19d1fccf41caaf273b6f7d0c4b38998dfdbedb0
Yolan10 <88558534+Yolan10@users.noreply.github.com>
2025-07-01 16:36:17 +0200

    Update api.py

--- api.py
+++ api.py
@@ -855,4 +855,7 @@ if __name__ == "__main__":
         create_user(sys.argv[2], sys.argv[3])
         print("User created")
     else:
-        socketio.run(app, debug=True, host="0.0.0.0", port=5000)
+        # Use production settings
+        port = int(os.environ.get("PORT", 5000))
+        debug = os.environ.get("FLASK_ENV") == "development"
+        socketio.run(app, debug=debug, host="0.0.0.0", port=port)
