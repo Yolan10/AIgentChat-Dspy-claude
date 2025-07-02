5a4dc57de122d074a0b1ae2dafdb9e049eeea151
Yolan10 <88558534+Yolan10@users.noreply.github.com>
2025-07-02 08:54:25 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-    sourcemap: false,
-    chunkSizeWarningLimit: 1000,
-  },
   server: {
-    proxy: {
+    proxy: process.env.NODE_ENV === 'development' ? {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true
@@ -23,6 +15,10 @@ export default defineConfig({
         changeOrigin: true,
         ws: true
       }
-    }
+    } : undefined
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 })
